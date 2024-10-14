7409870948f54215781c57e220b299a273ca2788
MostafaArafa01 <mostafaarafa478@gmail.com>
2024-10-14 15:45:44 +0300

    Solve tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -13,6 +13,11 @@ class Task extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'users_id');
+        return $this->belongsTo(User::class, 'users_id')->withDefault();
+    }
+    public function comments()
+    {
+        // TASK: add the code here for two-level relationship
+        return $this->hasMany(Comment::class);
     }
 }
